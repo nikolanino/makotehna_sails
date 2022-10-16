268d10cae978dda1e4f2a023b673c03ee4659346
nikolanino <nikolavnikolovski@gmail.com>
2022-10-17 00:30:33 +0200

    new slides images

--- assets/scripts/clientScript.js
+++ assets/scripts/clientScript.js
@@ -13,9 +13,9 @@
 
                 images: [
                     "../images/background.png",
-                    "https://cdn.pixabay.com/photo/2015/12/12/15/24/amsterdam-1089646_1280.jpg",
-                    "https://cdn.pixabay.com/photo/2016/02/17/23/03/usa-1206240_1280.jpg",
-                    "https://cdn.pixabay.com/photo/2016/12/04/19/30/berlin-cathedral-1882397_1280.jpg"
+                    "../images/slide.jpg",
+                    "../images/slide1.jpg",
+                    "../images/slide2.jpg"
                 ],
 
                 timer: null,
